43b7156aeead6cf02c692a53a0f7c61c145eb4d2
yannick <yannick.graal@gmail.com>
2023-03-09 09:09:00 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+class Schilderij {
+    public String titel;
+
+    public Schilderij(String titel) {
+        this.titel = titel;
+    }
+
+    class Veiling {
+        private ArrayList<Schilderij> schilderijenaanbod = new
+                ArrayList<>();
+        private ArrayList<Schilderij> verkochteSchilderijen = new
+                ArrayList<>();
+
+        public void verkoopSchilderij(String titel) {
+            for (Schilderij schilderij : schilderijenaanbod) {
+                verkochteSchilderijen.add(schilderij);
+                schilderijenaanbod.remove(schilderij);
+            }
+        }
+    }
+}
